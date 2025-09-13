8ee75a5e721e8f59dca39b3d59e93d034fb722bc
araitatsuya-code <rd0801577@gmail.com>
2025-09-13 22:56:17 +0900

    initial commit

---
+++ lib/japanese_business_days.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "japanese_business_days/version"
+
+module JapaneseBusinessDays
+  class Error < StandardError; end
+  # Your code goes here...
+end
